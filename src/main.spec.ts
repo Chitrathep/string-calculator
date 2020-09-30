cb2caa067df28f9a575a103014a969654a8a1c27
SaeedRahmatolahi <saeed.r@codium.co>
2020-09-30 14:07:30 +0700

    - Passes tests

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -41,4 +41,76 @@ describe('String Calculator', () => {
     // then
     expect(ans).toEqual(3);
   });
+
+  it('NO. 4', () => {
+    // given
+    const input = '23,1';
+
+    // when
+    const strCal = new StringCalculator()
+    const ans = strCal.add(input)
+
+    // then
+    expect(ans).toEqual(24);
+  });
+
+  it('NO. 5', () => {
+    // given
+    const input = '1\n2,3';
+
+    // when
+    const strCal = new StringCalculator()
+    const ans = strCal.add(input)
+
+    // then
+    expect(ans).toEqual(6);
+  });
+
+  it('NO. 6', () => {
+    // given
+    const input = '1,\n';
+
+    // when
+    const strCal = new StringCalculator()
+    const ans = strCal.add(input)
+
+    // then
+    expect(ans).toEqual(1);
+  });
+
+  it('NO. 7', () => {
+    // given
+    const input = '//;\n1;2';
+
+    // when
+    const strCal = new StringCalculator()
+    const ans = strCal.add(input)
+
+    // then
+    expect(ans).toEqual(3);
+  });
+
+  it('NO. 8', () => {
+    // given
+    const input = '2,1001';
+
+    // when
+    const strCal = new StringCalculator()
+    const ans = strCal.add(input)
+
+    // then
+    expect(ans).toEqual(2);
+  });
+
+  it('NO. 9', () => {
+    // given
+    const input = '//[***]\n1***2***3';
+
+    // when
+    const strCal = new StringCalculator()
+    const ans = strCal.add(input)
+
+    // then
+    expect(ans).toEqual(6);
+  });
 });
