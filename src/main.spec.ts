9ea2b67ef492db23e6a2d99557c9ce8a230e7a82
SaeedRahmatolahi <saeed.r@codium.co>
2020-09-30 13:52:03 +0700

    - Tests for all cases

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -17,4 +17,28 @@ describe('String Calculator', () => {
     // then
     expect(ans).toEqual(0);
   });
+
+  it('NO. 2', () => {
+    // given
+    const input = '3,9';
+
+    // when
+    const strCal = new StringCalculator()
+    const ans = strCal.add(input)
+
+    // then
+    expect(ans).toEqual(12);
+  });
+
+  it('NO. 3', () => {
+    // given
+    const input = '3';
+
+    // when
+    const strCal = new StringCalculator()
+    const ans = strCal.add(input)
+
+    // then
+    expect(ans).toEqual(3);
+  });
 });
